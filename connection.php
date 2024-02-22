34216ce09cf82ef6c1f2c24026bedca30d9ba7c0
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2024-02-22 02:57:40 -0300

    inicio do backendII

--- connection.php
+++ connection.php
@@ -6,9 +6,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
     $sobrenome = $_POST["sobrenome"];
     $aniversario = $_POST["aniversario"];
-    $sacerdocio = $_POST['sacerdocio'];
-    $quimbanda = $_POST['quimbanda'];
+    $sacerdocio = $_POST["sacerdocio"];
+    $quimbanda = $_POST["quimbanda"];
     $caboclo = $_POST["caboclo"];
 
+    //conecta ao banco de dados
+    $server = 'localhost';
+    $user = 'root';
+    $password = 'admin123';
+    $dbname = 'templo_de_nana';
+
+    $conexao = new mysqli($server, $user, $password, $dbname);
+
+    //verifica se há erro na conexão
+    if ($conexao->connect_error) {
+        die("Conexão Falhou" .$conexao->connect_error);
+    }
+
+    //prepara e executa a consulta SQL para inserir os dados
+    $sql = "INSERT INTO mediuns (
+        nome, 
+        sobrenome,
+        aniversario, 
+        sacerdocio,
+        quimbanda,
+        caboclo) 
+        
+        VALUES (
+        '$nome',
+        '$sobrenome',
+        '$aniversario',
+        '$sacerdocio',
+        '$quimbanda',
+        '$caboclo')";
+
+    if ($conexao->query($sql) === TRUE) {
+        echo "Dados inseridos com sucesso";
+
+    } else {
+        echo "Erro ao inserir dados: " . $conexao->error;
+    }
+
+    $conexao->close();
 
 }
