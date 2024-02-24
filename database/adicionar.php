b597dd213264b85df838983ef76bfc7a8be6b1c6
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2024-02-24 11:48:16 -0300

    inserção do campo email

--- database/adicionar.php
+++ database/adicionar.php
@@ -6,41 +6,53 @@ require_once "config.php";
 //recupera os dados do formulário
 $nome = filter_input(INPUT_POST, "nome");
 $sobrenome = filter_input(INPUT_POST, "sobrenome");
+$email = filter_input(INPUT_POST, "email");
 $aniversario = filter_input(INPUT_POST, "aniversario");
 $sacerdocio = filter_input(INPUT_POST, "sacerdocio");
 $quimbanda = filter_input(INPUT_POST, "quimbanda");
 $caboclo = filter_input(INPUT_POST, "caboclo");
 
-if ($nome && $sobrenome) {
-    
-    $sql = $pdo->prepare("INSERT INTO mediuns (
-        nome, 
-        sobrenome, 
-        aniversario, 
-        sacerdocio, 
-        quimbanda, 
-        caboclo)
-        
-        VALUES(
-        :nome,
-        :sobrenome,
-        :aniversario,
-        :sacerdocio,
-        :quimbanda,
-        :caboclo
-        )");
+if ($nome && $sobrenome && $email) {
 
-    $sql->bindValue(":nome", $nome);
-    $sql->bindValue(":sobrenome", $sobrenome);
-    $sql->bindValue(":aniversario", $aniversario);
-    $sql->bindValue(":sacerdocio", $sacerdocio);
-    $sql->bindValue(":quimbanda", $quimbanda);
-    $sql->bindValue(":caboclo", $caboclo);
-
+    $sql = $pdo->prepare("SELECT * FROM mediuns WHERE email = :email");
+    $sql->bindValue(":email", $email);
     $sql->execute();
 
-    header("Location: ../index.html");
-    exit;
+    if ($sql->rowCount() === 0) {
+    
+        $sql = $pdo->prepare("INSERT INTO mediuns (
+            nome, 
+            sobrenome, 
+            aniversario, 
+            sacerdocio, 
+            quimbanda, 
+            caboclo)
+            
+            VALUES(
+            :nome,
+            :sobrenome,
+            :aniversario,
+            :sacerdocio,
+            :quimbanda,
+            :caboclo
+            )");
+
+        $sql->bindValue(":nome", $nome);
+        $sql->bindValue(":sobrenome", $sobrenome);
+        $sql->bindValue(":aniversario", $aniversario);
+        $sql->bindValue(":sacerdocio", $sacerdocio);
+        $sql->bindValue(":quimbanda", $quimbanda);
+        $sql->bindValue(":caboclo", $caboclo);
+
+        $sql->execute();
+
+        header("Location: ../index.html");
+        exit;
+        
+    } else { 
+        header("Location: ../index.html");
+        exit;
+    }
 
 } else {
     header("Location: ../index.html");
