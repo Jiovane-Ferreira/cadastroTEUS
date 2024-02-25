fc2d98d4de61d059a94568e0494c6fe96e0a4ad8
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2024-02-25 07:03:46 -0300

    upadate 1/2 completo

--- editar.php
+++ editar.php
@@ -2,11 +2,31 @@
 
 require_once "database/config.php";
 
-$lista =[];
-$sql = $pdo->query("SELECT * FROM mediuns");
-if($sql->rowCount() > 0 ) {
+$info = [];
+$id = filter_input(INPUT_GET, 'id');
 
-    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+if ($id) {
+
+    $sql = $pdo->prepare("SELECT * FROM mediuns WHERE id = :id");
+    $sql->bindValue(":id", $id);
+    $sql->execute();
+
+    if($sql->rowCount() > 0 ) {
+
+        $info = $sql->fetch(PDO::FETCH_ASSOC);
+
+
+    } else {
+        
+        header("Location: index.php");
+        exit;
+
+    }
+
+
+} else {
+    header("Location: index.php");
+    exit;
 }
 
 ?>
@@ -32,88 +52,52 @@ if($sql->rowCount() > 0 ) {
                     <div class="linha-um" id="linha-um-form">
                         <div class="form-input" id="div-nome">
                             <label for="nome">Nome: </label>
-                            <input type="text" id="nome" name="nome"/>
+                            <input type="text" id="nome" name="nome" value="<?=$info['nome']; ?>"/>
                         </div>
                     
                         <div class="form-input" id="div-sobrenome">
                             <label for="sobrenome">Sobrenome: </label>
-                            <input type="text" id="sobrenome" name="sobrenome" />
+                            <input type="text" id="sobrenome" name="sobrenome" value="<?=$info['sobrenome']; ?>" />
                         </div>
                     </div>
 
                     <div class="linha-dois" id="linha-dois-form">
                         <div class="form-input" id="div-aniversario">
                             <label for="aniversario">Data de Aniversario: </label>
-                            <input type="date" id="aniversario" name="aniversario" />
+                            <input type="date" id="aniversario" name="aniversario" value="<?=$info['aniversario']; ?>"/>
                         </div>
 
                         <div class="form-input" id="div-email">
                             <label for="email">E-mail: </label>
-                            <input type="text" id="email" name="email" />
+                            <input type="text" id="email" name="email" value="<?=$info['email']; ?>"/>
                         </div>
                     </div>
                     
                     <div class="linha-tres" id="linha-tres-form">
                         <div class="form-input" id="div-sacerdócio">
                             <label form="sacerdocio">Possui Sacerdócio? </label>
-                            <input type="radio" id="sacerdocio_sim" name="sacerdocio" value="sim" checked /> Sim
-                            <input type="radio" id="sacerdocio_nao" name="sacerdocio" value="nao" /> Não
+                            <input type="radio" id="sacerdocio_sim" name="sacerdocio" value="sim" <?=$info['sacerdocio'] == 'sim' ? 'checked' : ''; ?> /> Sim
+                            <input type="radio" id="sacerdocio_não" name="sacerdocio" value="não" <?=$info['sacerdocio'] == 'nao' ? 'checked' : ''; ?> /> Não
                         </div>
 
                         <div class="form-input" id="div-cabolco">
                             <label for="caboclo">Caboclo-Chefe: </label>
-                            <input type="text" id="caboclo" name="caboclo" />
+                            <input type="text" id="caboclo" name="caboclo" value="<?=$info['caboclo']; ?>"/>
                         </div>
     
                     </div>
 
                     <div class="form-input" id="div-quimbanda">
                         <label form="quimbanda">Membro da Quimbanda? </label>
-                        <input type="radio" id="quimbanda_sim" name="quimbanda" value="sim" /> Sim
-                        <input type="radio" id="quimbanda_nao" name="quimbanda" value="nao" checked/> Não
+                        <input type="radio" id="quimbanda_sim" name="quimbanda" value="sim" <?=$info['quimbanda'] == 'sim' ? 'checked' : ''; ?> /> Sim
+                        <input type="radio" id="quimbanda_nao" name="quimbanda" value="nao" <?=$info['quimbanda'] == 'nao' ? 'checked' : ''; ?> /> Não
                     </div>
 
                     <div class="form-submit" id="div-submit">
-                        <input type="submit" value="Enviar" />  
+                        <input type="submit" value="Salvar" />  
                     </div>
 
                 </form>
-            </div>
-
-            <div class="container-table">
-                <table class="table-medium" id="table-medium">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nome</th>
-                            <th>Sobrenome</th>
-                            <th>Email</th>
-                            <th>Aniversario</th>
-                            <th>Sacerdocio</th>
-                            <th>Quimbanda</th>
-                            <th>Caboclo Chefe</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    
-                    <?php foreach($lista as $medium): ?>
-
-                        <tr>
-                            <td><?php echo $medium['id']; ?></td>
-                            <td><?=$medium['nome']; ?></td>
-                            <td><?=$medium['sobrenome']; ?></td>
-                            <td><?=$medium['email']; ?></td>
-                            <td><?=$medium['aniversario']; ?></td>
-                            <td><?=$medium['sacerdocio']; ?></td>
-                            <td><?=$medium['quimbanda']; ?></td>
-                            <td><?=$medium['caboclo']; ?></td>
-                            <td><a href="editar.php?id=<?=$medium['id']; ?>">[ Editar ]</a></td>
-                            <td><a href="excluir.php?id=<?=$medium['id']; ?>">[ Excluir ]</a></td>
-                        </tr>    
-                        
-                    <?php endforeach; ?>
-                    
-                </table>
             </div>
         </div>
     </div> 
